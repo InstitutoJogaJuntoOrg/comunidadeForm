7024e434850bf10f0f207d646693ae4880b780c7
ryanoliveiragit <ryanoliveirabrito178@gmail.com>
2025-03-06 12:23:36 -0300

    resultados

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -19,6 +19,21 @@ export const HomePage = () => {
              
             }}>Verifique seu email para atualizações.</p>
           </span>
+          <a className="buttonYellow"
+            style={{
+
+              borderRadius: "8px",
+              padding: "10px 20px",
+              textDecoration: "none",
+           
+            }}
+            target="_blank"
+            href="https://estaticos-ijj.s3.sa-east-1.amazonaws.com/Classifica%C3%A7%C3%A3o+Geral+Analise+de+dados+2025.1+-+Instituto+Joga+Junto.pdf"
+          >
+            <span style={{ color: "black", fontWeight: "bold" }}>
+              Confira os resultados
+            </span>
+          </a>
           {/* <Link
             to="/inscricao"
             style={{
