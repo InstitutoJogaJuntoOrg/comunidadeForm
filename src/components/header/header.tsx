7ad3cb08d8db0f066ccc11337c5b7f92cd9cfdfd
ryanoliveiragit <ryanoliveirabrito178@gmail.com>
2025-03-07 12:38:05 -0300

    feat: transparencia

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -78,6 +78,18 @@ export const Header = () => {
               Ajuda
             </a>
           </li>
+          <li>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "black",
+              }}
+              href="https://estaticos-ijj.s3.sa-east-1.amazonaws.com/CRIT%C3%89RIOS+DE+AVALIA%C3%87%C3%83O+E+SELE%C3%87%C3%83O+-+IJJ.pdf"
+              target={"_blank"}
+            >
+              Transparência 
+            </a>
+          </li>
         </ul>
         <div className="hamburger-icon" onClick={toggleLinks}>
           {showLinks ? <FaTimes /> : <FaBars />}
